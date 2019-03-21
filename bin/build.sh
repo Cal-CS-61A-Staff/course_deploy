215768ed2040a8cae18aed05a3d91576e2ac382e
Mehrdad Niknami <>
2019-03-21 02:16:31 -0700

    More build script polishing

--- bin/build.sh
+++ bin/build.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 main() {
 	test "$#" -eq 5 || { 1>&2 echo "Invalid number of arguments passed." && return 1; }
 
-	git fetch origin "$2"
-	git clean -q -d -f -x
-	git checkout -f "$2"
-	git clean -q -d -f -x
+	local make_args=(--no-print-directory -C src)
 
 	# Make sure pdflatex is accessible
-	export PATH="/usr/local/texlive/2017/bin/x86_64-linux:${PATH}"
-	#export INFOPATH="${INFOPATH}:/usr/local/texlive/2017/texmf-dist/doc/info"
-	#export MANPATH="${MANPATH}:/usr/local/texlive/2017/texmf-dist/doc/man"
+	local TEXLIVE_PATH="/usr/local/texlive/2017"
+	export PATH="${TEXLIVE_PATH}/bin/x86_64-linux:${PATH}"
+	#export INFOPATH="${TEXLIVE_PATH}/texmf-dist/doc/info:${INFOPATH}"
+	#export MANPATH="${TEXLIVE_PATH}/texmf-dist/doc/man:${MANPATH}"
 
-	# Run checks to determine if build is allowed. Checks will be skipped if
-	# the file does not exist and will be ignored if this is for a deploy build.
-	test ! -e scripts/checks.sh || bash scripts/checks.sh || test "$1" = "deploy"
-	test ! -e scripts/checks.py || python3 scripts/checks.py || test "$1" = "deploy"
+	set -x
 
-	local args=(--no-print-directory -C src)
-	make "${args[@]}" clean && { make "${args[@]}" all && make "${args[@]}" all && make "${args[@]}" -j1 all; }
+	git fetch    -q --prune origin "$2"
+	git clean    -q -d -f -x
+	git checkout -q    -f --detach origin/"$2"
+	git clean    -q -d -f -x
+
+	make "${make_args[@]}" all && make "${make_args[@]}" all && make "${make_args[@]}" -j1 all
 
 	rm -r -f -- "$4"
 	mv -T -- "$3" "$4"
