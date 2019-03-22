e0e31948ac721efa2f0f41eb23fdd11ca1310578
Mehrdad Niknami <>
2019-03-22 13:29:19 -0700

    Fix error handling

--- bin/build.sh
+++ bin/build.sh
@@ -6,6 +6,7 @@ main() {
 	test "$#" -eq 5 || { 1>&2 echo "Invalid number of arguments passed." && return 1; }
 
 	local make_args=(--no-print-directory -C src)
+	local buildtype="$1" branch="$2" output="$3" prtmp="$4" buildloc="$5"
 
 	# Make sure pdflatex is accessible
 	local TEXLIVE_PATH="/usr/local/texlive/2017"
@@ -15,16 +16,18 @@ main() {
 
 	set -x
 
-	git fetch    -q --prune origin "$2"
+	git fetch    -q --prune origin "${branch}"
 	git clean    -q -d -f -x
-	git checkout -q    -f --detach origin/"$2"
+	git checkout -q    -f --detach origin/"${branch}"
 	git clean    -q -d -f -x
 
 	make "${make_args[@]}" all && make "${make_args[@]}" all && make "${make_args[@]}" -j1 all
 
-	rm -r -f -- "$4"
-	mv -T -- "$3" "$4"
-	mv -T -- "$5" "$3"
+	rm -r -f -- "${prtmp}"
+	if [ -d "${output}" ]; then  # Don't move if it doesn't exist for some reason
+		mv -T -- "${output}" "${prtmp}" || true  # Can still fail due to a race condition, but ultimately this doesn't matter; what matters is that the output is updated
+	fi
+	mv -T -- "${buildloc}" "${output}"
 }
 
 main "$@"
